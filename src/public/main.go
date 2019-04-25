9aad0058e9b7472e81cd23459a3befbb83655125
Kradoss <kradoszhong@gmail.com>
2019-04-25 15:27:49 +0800

    1. use godotenv package 2. add .gitignore file 3. add .env file 4. build simple web server without any routes

--- src/public/main.go
+++ src/public/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// reload .env at the first time
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 func main() {
+	// if APP_ENV == "production" then set gin mode to release mode
+	if os.Getenv("APP_ENV") == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	// setting gin port
+	port := os.Getenv("PORT")
+
+	// create gin engine
+	router := gin.Default()
+
+	router.Run(":" + port)
 
 }
